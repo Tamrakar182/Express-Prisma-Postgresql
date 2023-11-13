1757e9058d681d1984c134869896d92119a28f3c
Tamrakar182 <manjultamrakar4@gmail.com>
2023-11-13 14:47:19 +0545

    simple prisma setup

--- index.js
+++ index.js
@@ -1,12 +1,30 @@
 import express from 'express';
+import dotenv from 'dotenv';
+import prisma from './db/prisma.js';
+
+dotenv.config();
 
 const app = express();
-const PORT = 8080;
 
-app.get('/', (req, res) => {
-    res.send('Hello World!');
+const PORT = 5000;
+
+app.get('/', async (req, res) => {
+    await prisma.user.create({
+        data: {
+            name: 'John Doe',
+            email: 'johndoe@email.com',
+            password: '123456789',
+        },
+    });   
+
+    const users = await prisma.user.findMany();
+
+    const names = users.map((user) => user.name);
+
+    res.send(`There are ${users.length} users in the database. Their names are: ${names.join(', ')}`);
+
 });
 
 app.listen(PORT, () => {
-    console.log(`Server listening on port ${PORT}`);
+    console.log(`Server listening on port ${PORT}: http://localhost:${PORT}`);
 });
